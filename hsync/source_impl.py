52b4adb70ceae7b09c0d313e7d32c87effa52879
Andre Lucas <andre@ae-35.com>
2014-10-06 16:03:24 +0100

    Factor out source_side slightly.

--- hsync/source_impl.py
+++ hsync/source_impl.py
@@ -23,7 +23,17 @@ log = logging.getLogger()
 
 
 def source_side(opt, args):
+    '''
+    Implement the source-side of hsync.
 
+    opt is the list of options from optparse, usually from the command line.
+
+    Constructs the signature URL from the information in opt.
+
+    args is ignored.
+
+    Calls out to source_generate() to build the new HSYNC.SIG* file.
+    '''
     # If there's an existing hashfile, optionally use it to try to reduce the
     # amount of scanning.
 
@@ -49,44 +59,67 @@ def source_side(opt, args):
         abs_hashfile = os.path.join(opt.source_dir, hashfile)
         log.debug("Synthesised hash file location: '%s'", abs_hashfile)
 
-    existing_hl = None
-
     # Lock the source-side hash file.
     abs_lockfile = abs_hashfile + '.lock'
+
+    # Generate the new hashfile.
     with LockFileManager(abs_lockfile):
+        return source_generate(abs_hashfile, opt)
 
-        if not opt.always_checksum and os.path.exists(abs_hashfile):
-            if not opt.quiet:
-                print("Reading existing hashfile")
 
-            # Fetch the signature file.
-            hashfile_contents = fetch_contents('file://' + abs_hashfile, opt, short_name=opt.hash_file)
+def _read_hashlist(abs_hashfile, opt):
+    # Fetch the signature file.
+    hashfile_contents = fetch_contents('file://' + abs_hashfile, opt,
+                                                short_name=opt.hash_file)
 
-            if opt.compress_signature:
-                gziptmp = tempfile.NamedTemporaryFile()
-                with gziptmp:
-                    gziptmp.file.write(hashfile_contents)
-                    gziptmp.file.close()
-                    strfile = []
-                    for l in gzip.open(gziptmp.name):
-                        strfile.append(l.rstrip())
+    if opt.compress_signature:
+        gziptmp = tempfile.NamedTemporaryFile()
+        with gziptmp:
+            gziptmp.file.write(hashfile_contents)
+            gziptmp.file.close()
+            strfile = []
+            for l in gzip.open(gziptmp.name):
+                strfile.append(l.rstrip())
 
-            else:
-                strfile = hashfile_contents.splitlines()
+    else:
+        strfile = hashfile_contents.splitlines()
 
-            existing_hl = hashlist_from_stringlist(strfile, opt, root=opt.source_dir)
+    existing_hl = hashlist_from_stringlist(strfile, opt, root=opt.source_dir)
 
-        hashlist = hashlist_generate(opt.source_dir, opt, existing_hashlist=existing_hl)
-        if hashlist is not None:
 
-            write_success = sigfile_write(hashlist, abs_hashfile, opt, use_tmp=True)
-            if not write_success:
-                log.error("Failed to write signature file '%s'",
-                    os.path.join(opt.source_dir, opt.hash_file))
-                return False
+def source_generate(abs_hashfile, opt):
+    '''
+    
+    Generate a hash of the local filesystem, optionally using existing
+    signatures to speed up the process.
 
-            return True
+    '''
 
-        else:
-            log.error("Send-side generate failed")
+    existing_hl = None
+
+    if not opt.always_checksum and os.path.exists(abs_hashfile):
+        if not opt.quiet:
+            print("Reading existing hashfile")
+
+        # Fetch the signature file.
+        hashfile_contents = fetch_contents('file://' + abs_hashfile, opt,
+                                            short_name=opt.hash_file)
+        # Turn into a hashlist.
+        existing_hl = _read_hashlist(abs_hashfile, opt)
+
+    hashlist = hashlist_generate(opt.source_dir, opt,
+                                    existing_hashlist=existing_hl)
+
+    if hashlist is not None:
+
+        write_success = sigfile_write(hashlist, abs_hashfile, opt, use_tmp=True)
+        if not write_success:
+            log.error("Failed to write signature file '%s'",
+                os.path.join(opt.source_dir, opt.hash_file))
             return False
+
+        return True
+
+    else:
+        log.error("Send-side generate failed")
+        return False
