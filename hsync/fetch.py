8efacaf9106bf0a9c039f0f1be3d3293df77f040
Andre Lucas <andre@ae-35.com>
2014-10-08 09:07:38 +0100

    Lint pass 1 of fetch.py.

--- hsync/fetch.py
+++ hsync/fetch.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+from BaseHTTPServer import BaseHTTPRequestHandler
 import hashlib
 import logging
 import os
@@ -10,17 +11,17 @@ import sys
 import urllib2
 import urlparse
 
-from idmapper import UidGidMapper
 from numformat import IECUnitConverter
 from stats import StatsCollector
+from exceptions import *
 
 log = logging.getLogger()
 
 
 def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
-                    short_name=None, file_count_number=None,
-                    file_count_total=None, remote_flag=True,
-                    include_in_total=True):
+                   short_name=None, file_count_number=None,
+                   file_count_total=None, remote_flag=True,
+                   include_in_total=True):
     '''
     Wrap a fetch, which may be from a file or URL depending on the options.
 
@@ -30,7 +31,7 @@ def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
     fullpath = fpath
 
     if not no_trim and opts.trim_path and root:
-        fullpath = os.path.join(opt.source_url, fpath)
+        fullpath = os.path.join(opts.source_url, fpath)
 
     log.debug("fetch_contents: %s", fullpath)
 
@@ -48,11 +49,11 @@ def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
     filecountstr = ''
     if file_count_number is not None:
         if file_count_total is not None:
-            pct = 100.0*file_count_number/file_count_total
+            pct = 100.0 * file_count_number / file_count_total
             filecountstr = ' [object %d/%d (%.0f%%)]' % (
-                                                file_count_number,
-                                                file_count_total,
-                                                pct)
+                file_count_number,
+                file_count_total,
+                pct)
         else:
             filecountstr = ' [file %d]' % file_count_number
 
@@ -69,7 +70,6 @@ def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
         print('\rF: %s%s%s' % (fname, progress_spacer, pfx), end='')
 
     outfile = ''
-    progress = False
 
     try:
         if remote_flag and include_in_total:
@@ -99,12 +99,10 @@ def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
         size_is_known = True
 
     if opts.progress and size_is_known:
-        progress = True
         sizestr = IECUnitConverter.bytes_to_unit(size)
 
     bytes_read = 0
     more_to_read = True
-    last_strlen = 0
 
     def progstr():
         if size_is_known:
@@ -113,16 +111,18 @@ def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
             else:
                 pct = 100.0 * bytes_read / size
 
-            print ("\r\t%s (file progress %s/%s [%.0f%%])" % (pfx,
-                                IECUnitConverter.bytes_to_unit(bytes_read),
-                                sizestr,
-                                pct),
-                    end='')
+            print ("\r\t%s (file progress %s/%s [%.0f%%])" %
+                   (pfx,
+                    IECUnitConverter.bytes_to_unit(
+                        bytes_read),
+                    sizestr,
+                    pct),
+                   end='')
         else:
-            print("\r%s (download %s/unknown)" % (pfx,
-                                IECUnitConverter.bytes_to_unit(bytes_read)),
-                    end='')
-
+            print("\r%s (download %s/unknown)" %
+                  (pfx,
+                   IECUnitConverter.bytes_to_unit(bytes_read)),
+                  end='')
 
     if opts.progress:
         progstr()
@@ -159,16 +159,22 @@ def fetch_contents(fpath, opts, root='', no_trim=False, for_filehash=None,
     if size_is_known and bytes_read != size:
         # That's an error. No need for a cryptochecksum to tell that.
         log.warn("'%s': Fetched %d bytes, expected %d bytes",
-                    fname, bytes_read, size)
+                 fname, bytes_read, size)
         return None
 
     return outfile
 
 
-class FetchException(Exception): pass
-class FetchFailedChecksumException(Exception): pass
+class FetchException(Exception):
+    pass
 
-class FetchFatalException(Exception): pass
+
+class FetchFailedChecksumException(Exception):
+    pass
+
+
+class FetchFatalException(Exception):
+    pass
 
 
 def fetch_needed(needed, source, opts):
@@ -183,7 +189,6 @@ def fetch_needed(needed, source, opts):
     '''
 
     r = SystemRandom()
-    mapper = UidGidMapper()
     fetch_added = []
     error_count = 0
 
@@ -191,10 +196,10 @@ def fetch_needed(needed, source, opts):
         source += "/"
 
     counters = StatsCollector('FetchStats', [
-                    'contents_differ_count',
-                    'differing_file_index',
-                    'signature_checkpoint'
-                    ])
+        'contents_differ_count',
+        'differing_file_index',
+        'signature_checkpoint'
+    ])
     counters.contents_differ_count = 0
     counters.differing_file_index = 0
     counters.signature_checkpoint = 0
@@ -207,7 +212,7 @@ def fetch_needed(needed, source, opts):
     # hashlist. That way, current information is written to the client
     # HSYNC.SIG, saving on re-scans.
     changed = StatsCollector('ChangeStatus',
-                                ['contents', 'uidgid', 'mode', 'mtime'])
+                             ['contents', 'uidgid', 'mode', 'mtime'])
 
     for n, fh in enumerate(needed, start=1):
 
@@ -229,21 +234,21 @@ def fetch_needed(needed, source, opts):
                 _file_fetch(fh, source_url, changed, counters, r, opts)
 
             except FetchException as e:
-                warn("Fetch file %s failed: %s", source_url, e)
+                log.warn("Fetch file %s failed: %s", source_url, e)
                 error_count += 1
 
         elif fh.is_link:
             try:
                 _link_fetch(fh, changed, opts)
             except FetchException as e:
-                warn("Update link %s failed: %s", fh.fpath, e)
+                log.warn("Update link %s failed: %s", fh.fpath, e)
                 error_count += 1
 
         elif fh.is_dir:
             try:
                 _dir_fetch(fh, changed, opts)
             except FetchException as e:
-                warn("Update dir %s failed: %s", fh.fpath, e)
+                log.warn("Update dir %s failed: %s", fh.fpath, e)
                 error_count += 1
 
         # Update the client-side HSYNC.SIG data.
@@ -283,7 +288,7 @@ def delete_not_needed(not_needed, target, opts):
             try:
                 os.remove(fullpath)
             except OSError as e:
-                warn("Failed to delete file %s: %s", fullpath, e)
+                log.warn("Failed to delete file %s: %s", fullpath, e)
                 error_count += 1
 
     if dirs_to_delete:
@@ -298,7 +303,7 @@ def delete_not_needed(not_needed, target, opts):
             try:
                 os.rmdir(fullpath)
             except OSError as e:
-                warn("Failed to delete dir %s: %s", fullpath, e)
+                log.warn("Failed to delete dir %s: %s", fullpath, e)
                 error_count += 1
 
     if error_count:
@@ -312,7 +317,7 @@ def update_dest_filehash(src_fh, changed, fetch_added):
 
     if src_fh.associated_dest_object is None:
         log.debug("Saved new file '%s' for inclusion in sigfile",
-                    src_fh.fpath)
+                  src_fh.fpath)
         fetch_added.append(src_fh)
 
     else:
@@ -346,15 +351,17 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
         opts.stats.file_contents_differed += 1
 
         log.debug("Fetching: '%s' dest_missing %s contents_differ %s",
-            fh.fpath, fh.dest_missing, fh.contents_differ)
+                  fh.fpath, fh.dest_missing, fh.contents_differ)
 
         if not opts.quiet and sys.stdout.isatty():
             print("F: %s" % fh.fpath, end='')
 
         # Fetch_contents will display progress information itself.
-        contents = fetch_contents(source_url, opts, for_filehash=fh,
-            file_count_number=counters.differing_file_index,
-            file_count_total=counters.contents_differ_count)
+        contents = fetch_contents(
+                            source_url, opts,
+                            for_filehash=fh,
+                            file_count_number=counters.differing_file_index,
+                            file_count_total=counters.contents_differ_count)
 
         if contents is None:
             if opts.fail_on_errors:
@@ -362,13 +369,13 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
                     "Failed to fetch '%s'" % source_url)
             else:
                 log.debug("Failed to fetch '%s'",
-                            fh.fpath)
+                          fh.fpath)
                 raise FetchContentsFailedException(
                     "Failed to fetch %s" % source_url)
 
         else:
 
-            changed.contents = True # If we fetched it, we changed it.
+            changed.contents = True  # If we fetched it, we changed it.
 
             chk = hashlib.sha256()
             log.debug("Hashing contents")
@@ -384,7 +391,7 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
 
             if chk.hexdigest() != fh.hashstr:
                 log.warn("File '%s' failed checksum verification!",
-                            fh.fpath)
+                         fh.fpath)
                 raise FetchFailedChecksumException(
                     "File %s failed checksum verification" % fh.fpath)
 
@@ -400,12 +407,12 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
                         tgt_file)
 
             # Dealing with file descriptors, use os.f*() variants.
-            tgt = os.open(tgt_file_rnd, 
-                            os.O_CREAT | os.O_EXCL | os.O_WRONLY,
-                            fh.mode)
+            tgt = os.open(tgt_file_rnd,
+                          os.O_CREAT | os.O_EXCL | os.O_WRONLY,
+                          fh.mode)
             if tgt == -1:
                 raise OSOperationFailedError("Failed to open '%s'",
-                        tgt_file_rnd)
+                                             tgt_file_rnd)
 
             expect_uid = fh.uid
             expect_gid = fh.gid
@@ -415,29 +422,28 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
                     filestat.st_gid != expect_gid:
                 changed.uidgid = True
                 log.debug("Changing file %s ownership to %s/%s",
-                            tgt_file_rnd, fh.user, fh.group)
+                          tgt_file_rnd, fh.user, fh.group)
                 if os.fchown(tgt, expect_uid, expect_gid) == -1:
                     log.warn("Failed to fchown '%s' to user %s "
-                                "group %s",
-                                tgt_file_rnd, fh.user, fh.group)
+                             "group %s",
+                             tgt_file_rnd, fh.user, fh.group)
 
-            changed.mode = False # We didn't change it, we created it.
+            changed.mode = False  # We didn't change it, we created it.
 
             os.write(tgt, contents)
             os.close(tgt)
             log.debug("Moving into place: '%s' -> '%s'",
-                        tgt_file_rnd, tgt_file)
+                      tgt_file_rnd, tgt_file)
             if os.rename(tgt_file_rnd, tgt_file) == -1:
                 raise OSOperationFailedError(
-                        "Failed to rename '%s' to '%s'",
-                        tgt_file_rnd, tgt_file)
+                    "Failed to rename '%s' to '%s'",
+                    tgt_file_rnd, tgt_file)
 
             changed.mtime = True
             os.utime(tgt_file, (fh.mtime, fh.mtime))
 
             if changed.uidgid or changed.mtime or changed.mode:
                 opts.stats.file_metadata_differed += 1
-
 
     elif fh.metadata_differs:
 
@@ -456,38 +462,36 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
             changed.uidgid = True
             if not opts.quiet:
                 print(" (user/group: %s/%s -> %s/%s)" % (
-                                    filestat.st_uid, filestat.st_gid,
-                                    expect_uid, expect_gid), end='')
+                    filestat.st_uid, filestat.st_gid,
+                    expect_uid, expect_gid), end='')
             log.debug("Changing file %s ownership to %s/%s",
-                        tgt_file, fh.user, fh.group)
+                      tgt_file, fh.user, fh.group)
             if os.chown(tgt_file, expect_uid, expect_gid) == -1:
                 log.warn("Failed to fchown '%s' to user %s group %s",
-                        tgt_file, fh.user, fh.group)
+                         tgt_file, fh.user, fh.group)
 
         if filestat.st_mode != fh.mode:
             changed.mode = True
             if not opts.quiet:
-                print(" (mode %06o -> %06o)" % 
-                        (filestat.st_mode, fh.mode), end='')
+                print(" (mode %06o -> %06o)" %
+                      (filestat.st_mode, fh.mode), end='')
 
             log.debug("'%s': Setting mode: %06o", fh.fpath, fh.mode)
             if os.chmod(tgt_file, fh.mode) == -1:
                 log.warn("Failed to fchmod '%s' to %06o",
-                            fh.fpath, fh.mode)
+                         fh.fpath, fh.mode)
 
         if filestat.st_mtime != fh.mtime:
             changed.mtime = True
             if not opts.quiet:
-                print(" (mtime %d -> %d)" % 
-                        (filestat.st_mtime, fh.mtime), end='')
+                print(" (mtime %d -> %d)" %
+                      (filestat.st_mtime, fh.mtime), end='')
             os.utime(tgt_file, (fh.mtime, fh.mtime))
 
         opts.stats.file_metadata_differed += 1
 
         if not opts.quiet:
             print('')
-
-
 
     elif fh.mtime_differs:
         log.debug("'%s': mtime differs", fh.fpath)
@@ -496,7 +500,7 @@ def _file_fetch(fh, source_url, changed, counters, random, opts):
         if not opts.quiet:
             filestat = os.stat(tgt_file)
             print("F: %s (mtime %d -> %d)" % (fh.fpath,
-                                filestat.st_mtime, fh.mtime))
+                                              filestat.st_mtime, fh.mtime))
 
         os.utime(tgt_file, (fh.mtime, fh.mtime))
         opts.stats.file_metadata_differed += 1
@@ -508,10 +512,10 @@ def _link_fetch(fh, changed, opts):
 
     if not os.path.exists(linkpath):
         log.debug("Creating symlink '%s'->'%s'",
-                    linkpath, fh.link_target)
+                  linkpath, fh.link_target)
         if not opts.quiet:
             print("L: (create) %s -> %s" %
-                    (linkpath, fh.link_target))
+                  (linkpath, fh.link_target))
         os.symlink(fh.link_target, linkpath)
         opts.stats.link_contents_differed += 1
 
@@ -529,10 +533,10 @@ def _link_fetch(fh, changed, opts):
             if curtgt != fh.link_target:
                 changed.contents = True
                 log.debug("Moving symlink '%s'->'%s'",
-                             linkpath, dh.link_target)
+                          linkpath, dh.link_target)
                 if not opts.quiet:
                     print("L: (move) %s -> %s" %
-                            (linkpath, fh.link_target))
+                          (linkpath, fh.link_target))
                 os.symlink(fh.link_target, linkpath)
                 opts.stats.link_contents_differed += 1
 
@@ -543,13 +547,13 @@ def _link_fetch(fh, changed, opts):
     if lstat.st_uid != expect_uid or lstat.st_gid != expect_gid:
         changed.uidgid = True
         log.debug("Changing link '%s' ownership to %s/%s",
-            linkpath, expect_uid, expect_gid)
+                  linkpath, expect_uid, expect_gid)
         os.lchown(linkpath, expect_uid, expect_gid)
 
     if lstat.st_mode != fh.mode:
         changed.mode = True
         log.debug("Changing link '%s' mode to %06o",
-                    linkpath, fh.mode)
+                  linkpath, fh.mode)
         os.lchmod(linkpath, fh.mode)
 
     if changed.mode or changed.uidgid:
@@ -583,11 +587,11 @@ def _dir_fetch(fh, changed, opts):
     if dstat.st_uid != expect_uid or dstat.st_gid != expect_gid:
         changed.uidgid = True
         log.debug("Changing dir %s ownership to %s/%s",
-                    tgt_dir, fh.user, fh.group)
+                  tgt_dir, fh.user, fh.group)
         if not opts.quiet:
-            print("D: %s (user/group: %s/%s -> %s/%s)" % 
-                    (fh.fpath, dstat.st_uid, dstat.st_gid, expect_uid,
-                     expect_gid))
+            print("D: %s (user/group: %s/%s -> %s/%s)" %
+                  (fh.fpath, dstat.st_uid, dstat.st_gid, expect_uid,
+                   expect_gid))
         os.chown(tgt_dir, expect_uid, expect_gid)
 
     if dstat.st_mode != fh.mode:
@@ -595,9 +599,8 @@ def _dir_fetch(fh, changed, opts):
         log.debug("Changing dir %s mode to %06o", tgt_dir, fh.mode)
         if not opts.quiet:
             print("D: %s (mode: %06o -> %06o)" % (fh.fpath,
-                    dstat.st_mode, fh.mode))
+                                                  dstat.st_mode, fh.mode))
         os.chmod(tgt_dir, fh.mode)
 
     if changed.mode or changed.uidgid:
         opts.stats.directory_metadata_differed += 1
-
