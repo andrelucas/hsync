f838e52a87480b307d6f34de41db874f0ad6c5b9
Andre Lucas <andre@ae-35.com>
2014-10-20 10:35:59 +0100

    No '^' for file exclude regex.

--- hsync/filehash.py
+++ hsync/filehash.py
@@ -46,7 +46,7 @@ class BadSymlinkFormatError(HashStringFormatError):
 
 fileignore = [
     re.compile(r'(~|\.swp)$'),
-    re.compile(r'^\.nfs\[0-9a-f]+$'),
+    re.compile(r'\.nfs[0-9a-f]+$'),
 ]
 
 dirignore = [
